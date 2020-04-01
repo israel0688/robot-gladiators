6879aae22e0b0f6d001e3ac6401803d5c19a58c3
Israel Berlanga <israel0688@hotmail.com>
2020-03-31 20:42:35 -0500

    end of 3.2.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,14 @@
 var playerName = window.prompt("What is your robots's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-//you can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android","Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var playerMoney = 10;
-
-var fight = function()
+var fight = function(enemyName)
 {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -65,4 +62,9 @@ if (promptFight === "fight" || promptFight === "FIGHT")
 
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++)
+{
+    fight(enemyNames[i]);
+}
+
+//fight();
