31aa47a2e41d6880116a9c9222f497b8bc23a5d0
Israel Berlanga <israel0688@hotmail.com>
2020-04-03 20:14:22 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney -10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      //generate random damage valu based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -48,7 +49,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -73,6 +75,8 @@ var startGame = function()
     playerAttack = 10;
     playerMoney = 10;
 
+
+
 for(var i = 0; i < enemyNames.length; i++)
 {
     if (playerHealth > 0) 
@@ -84,7 +88,7 @@ for(var i = 0; i < enemyNames.length; i++)
         var pickedEnemyName = enemyNames[i];
     
         // reset enemyHealth before starting new fight
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
     
         // use debugger to pause script from running and check what's going on at that moment in the code
         // debugger;
@@ -181,7 +185,16 @@ var shop = function()
       shop();
       break;
   }
+
 };
+
+var randomNumber = function(min, max)
+ {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+  };
+
 
 startGame();
 //fight();
